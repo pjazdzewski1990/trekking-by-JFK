59715280217d1c2fa0ad54c422c1c021509dbcb9
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2011-11-07 09:09:23 +0100

    Ajax rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ m1 = Marker.create([{name:'marker1', posX:27.173006, posY:78.042086, url:'desc/m
 m1 = Marker.create([{name:'marker2', posX:25.173006, posY:80.042086, url:'desc/marker2'}])
 m1 = Marker.create([{name:'marker3', posX:30.173006, posY:75.042086, url:'desc/marker3'}])
 m1 = Marker.create([{name:'marker4', posX:26.173006, posY:83.042086, url:'desc/marker4'}])
-m1 = Marker.create([{name:'marker5', posX:28.173006, posY:76.042086, url:'desc/marker1'}])
-m1 = Marker.create([{name:'marker6', posX:29.173006, posY:81.042086, url:'desc/marker2'}])
-m1 = Marker.create([{name:'marker7', posX:31.173006, posY:77.042086, url:'desc/marker3'}])
-m1 = Marker.create([{name:'marker8', posX:24.173006, posY:75.842086, url:'desc/marker4'}])
+m1 = Marker.create([{name:'marker5', posX:28.173006, posY:76.042086, url:'desc/marker5'}])
+m1 = Marker.create([{name:'marker6', posX:29.173006, posY:81.042086, url:'desc/marker6'}])
+m1 = Marker.create([{name:'marker7', posX:31.173006, posY:77.042086, url:'desc/marker7'}])
+m1 = Marker.create([{name:'marker8', posX:24.173006, posY:75.842086, url:'desc/marker8'}])
 puts "Liczba markerow " + Marker.count.to_s
